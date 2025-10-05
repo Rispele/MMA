9291cd60bd89196843726114ce357c177982ebd7
d.smirnov <d.smirnov@kontur.ru>
2025-10-05 20:12:37 +0500

    feat: improve client

--- Sources/Application.Clients/RoomsClient.cs
+++ Sources/Application.Clients/RoomsClient.cs
@@ -4,6 +4,7 @@ using Application.Clients.Dtos.Requests.RoomPatching;
 using Application.Clients.Dtos.Requests.RoomsQuerying;
 using Application.Clients.Dtos.Responses;
 using Application.Clients.Dtos.Room;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Application.Clients;
 
@@ -12,7 +13,7 @@ public class RoomsClient(HttpClient httpClient) : IRoomsClient
     public async Task<RoomDto> GetRoomByIdAsync(int roomId, CancellationToken cancellationToken = default)
     {
         var room = await httpClient.GetFromJsonAsync<RoomDto>($"rooms/{roomId}", cancellationToken);
-        
+
         return room!;
     }
 
@@ -29,11 +30,15 @@ public class RoomsClient(HttpClient httpClient) : IRoomsClient
     public async Task<RoomDto> CreateRoomAsync(CreateRoomRequest request, CancellationToken cancellationToken = default)
     {
         var response = await httpClient.PostAsJsonAsync("rooms", request, cancellationToken);
-        response.EnsureSuccessStatusCode();
-        
-        var room = await response.Content.ReadFromJsonAsync<RoomDto>(cancellationToken: cancellationToken);
 
-        return room!;
+        if (response.IsSuccessStatusCode)
+        {
+            return (await response.Content.ReadFromJsonAsync<RoomDto>(cancellationToken: cancellationToken))!;
+        }
+
+        var problemDetails = await response.Content.ReadFromJsonAsync<ProblemDetails>(cancellationToken);
+
+        throw new ApiException(problemDetails);
     }
 
     public async Task PatchRoomAsync(int roomId, PatchRoomRequest patch, CancellationToken cancellationToken = default)
---
+++ Sources/Commons/EnumerableExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Commons;
+
+public static class EnumerableExtensions
+{
+    public static string JoinStrings<TValue>(this IEnumerable<TValue> enumerable, string delimiter)
+    {
+        return string.Join(delimiter, enumerable);
+    }
+}
