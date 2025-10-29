b56ff836c7d638e88171dc6a20d24cf180601750
d.smirnov <d.smirnov@kontur.ru>
2025-10-30 02:37:14 +0500

    wip

---
+++ Sources/WebApi/ModelConverters/IRoomConverter.cs
@@ -0,0 +1,11 @@
+﻿using Mapster;
+using Rooms.Core.Dtos.Room;
+using WebApi.Models.Room;
+
+namespace WebApi.ModelConverters;
+
+[Mapper]
+public interface IRoomConverter
+{
+    public RoomModel MapTo(RoomDto room);
+}
