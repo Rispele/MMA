5936630fdbf7128b2a14816c495659b9ba73ca63
d.smirnov <d.smirnov@kontur.ru>
2025-12-10 01:49:30 +0500

    fix

--- Sources/WebApi.Core/Models/BookingRequest/RoomEventCoordinator/IRoomEventCoordinatorModel.cs
+++ Sources/WebApi.Core/Models/BookingRequest/RoomEventCoordinator/IRoomEventCoordinatorModel.cs
@@ -1,8 +1,13 @@
-﻿using Booking.Domain.Propagated.BookingRequests;
+﻿using System.Text.Json.Serialization;
+using Booking.Domain.Propagated.BookingRequests;
 
 namespace WebApi.Core.Models.BookingRequest.RoomEventCoordinator;
 
+[JsonDerivedType(typeof(OtherRoomEventCoordinatorModel), nameof(OtherRoomEventCoordinatorModel))]
+[JsonDerivedType(typeof(ScientificRoomEventCoordinatorModel), nameof(ScientificRoomEventCoordinatorModel))]
+[JsonDerivedType(typeof(StudentRoomEventCoordinatorModel), nameof(StudentRoomEventCoordinatorModel))]
 public interface IRoomEventCoordinatorModel
 {
+    [JsonPropertyName("$type")]
     public RoomEventCoordinatorType EventCoordinatorType { get; }
 }
