a4ca1cf7eb4816bd7a89a520221e70e5370221a4
d.smirnov <d.smirnov@kontur.ru>
2026-01-18 00:05:56 +0500

    fix: make schedule status non nullable

---
+++ Sources/Booking.Infrastructure/Migrations/20260117190522_tt.cs
@@ -0,0 +1,48 @@
+﻿using Booking.Domain.Propagated.BookingRequests;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Booking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class tt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<BookingScheduleStatus>(
+                name: "booking_schedule_status",
+                table: "booking_requests",
+                type: "booking_schedule_status",
+                nullable: false,
+                defaultValue: (BookingScheduleStatus)0,
+                oldClrType: typeof(BookingScheduleStatus),
+                oldType: "booking_schedule_status",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int[]>(
+                name: "room_ids",
+                table: "booking_requests",
+                type: "integer[]",
+                nullable: false,
+                defaultValue: new int[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "room_ids",
+                table: "booking_requests");
+
+            migrationBuilder.AlterColumn<BookingScheduleStatus>(
+                name: "booking_schedule_status",
+                table: "booking_requests",
+                type: "booking_schedule_status",
+                nullable: true,
+                oldClrType: typeof(BookingScheduleStatus),
+                oldType: "booking_schedule_status");
+        }
+    }
+}
